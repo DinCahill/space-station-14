a4078672a8927ad0dd8f5770c48d067840b498d8
DinCahill <magicmole@gmail.com>
2020-09-03 01:16:56 +0100

    Use pathlib and sys.executable in RUN_THIS.py
    Run git_helper.py using the current Python binary from sys.executable. Removes the Windows check.

--- RUN_THIS.py
+++ RUN_THIS.py
@@ -5,14 +5,13 @@ from __future__ import print_function
 import sys
 import subprocess
 
-IS_WINDOWS = sys.platform in ("win32", "cygwin")
-
 version = sys.version_info
 if version.major < 3 or (version.major == 3 and version.minor < 5):
     print("ERROR: You need at least Python 3.5 to build SS14.")
     sys.exit(1)
 
-if IS_WINDOWS:
-    subprocess.run(["py", "-3", "git_helper.py"], cwd="BuildChecker")
-else:
-    subprocess.run(["python3", "git_helper.py"], cwd="BuildChecker")
+from pathlib import Path
+this_scripts_directory = Path(__file__).parent
+bc_path = this_scripts_directory/"BuildChecker"
+
+subprocess.run([sys.executable, "git_helper.py"], cwd=bc_path)
